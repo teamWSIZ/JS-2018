2f2b3c68c00f6b5aa4fc855953eacfa9d3df5ab8
P Marecki <pmarecki.pro@gmail.com>
2019-02-01 17:23:13 +0100

    Some thoughts

--- static/js/controllers/teacherCtrl.js
+++ static/js/controllers/teacherCtrl.js
@@ -6,7 +6,11 @@ angular.module('myApp.controllers').controller('teacherCtrl',
             console.log('Loading `dochody` controller');
             $scope.M = {};
             $scope.wynik = [];
+            $scope.testAlias = 'dd';
+            $scope.tablica = [1, 2, 3, 4, 5];
+
             const URL = "http://basra.wsi.edu.pl:1111";
+
 
             $scope.test = {};
 
@@ -26,12 +30,36 @@ angular.module('myApp.controllers').controller('teacherCtrl',
                 $scope.test.items.push({"from": "", "to": ""});
             };
 
+            let randomIndex = function (max) {
+                return Math.floor(Math.random() * max);
+            };
+
+            $scope.shuffleTable = function (arr) {
+                const n = arr.length;
+                console.log('before: ' + arr);
+                for (let i = 0; i < 100; i++) {
+                    const from = randomIndex(n);
+                    const to = randomIndex(n);
+                    console.log('from:' + from + ' to:' + to);
+                    const t = arr[from];
+                    arr[from] = arr[to];
+                    arr[to] = t;
+                }
+                console.log('after: ' + arr);
+            };
+
+            $scope.deleteQuestion = function (qu) {
+                let idx = $scope.test.items.indexOf(qu);
+                console.log('Removing question at index:' + idx);
+                $scope.test.items.splice(idx,1);
+            };
+
             $scope.loadTest = function () {
                 $http({
                     url: URL + '/tests',
                     method: 'GET',
                     params: {
-                        alias: "dd"
+                        alias: $scope.testAlias
                     }
                 }).success(function (dane) {
                     $scope.test = dane;
